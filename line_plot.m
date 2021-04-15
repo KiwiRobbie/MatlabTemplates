a940f3e0b083ba7bb511abf8cd1504f5dbececa8
KiwiRobbie <r.b.christie@icloud.com>
2021-04-15 18:15:38 +1200

    added plotting examples

---
+++ line_plot.m
@@ -0,0 +1,11 @@
+x=1:10;
+y=rand(3,10)
+
+
+figure(1);
+clf;
+plot(x,y);
+title('I will not eat poop in class')
+ylabel('Poop')
+xlabel('Class')
+legend('No', 'eat', 'poop')
---
+++ scatter_plot.m
@@ -0,0 +1,8 @@
+x=rand(1,10);
+y=rand(1,10);
+figure(1);
+clf;
+scatter(x,y,'ro');
+title('I will not eat poop in class')
+ylabel('poop')
+xlabel('class')
